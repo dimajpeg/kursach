4e138f6c362d5ac891e20f1baaeaf2576c8b974b
dima <>
2024-06-04 19:05:02 +0300

    First commit

---
+++ src/test/java/org/example/kursach1/Kursach1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kursach1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kursach1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
